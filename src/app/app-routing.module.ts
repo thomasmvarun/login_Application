0b65f9185e71eebc54687c99b4c2ba7b802e520e
Admin <admin@Admins-MBP.fritz.box>
2019-09-08 09:36:40 +1200

    homescreen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {AppComponent} from './app.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 const routes: Routes = [
   {
     path:'',
     component:LoginComponent
+  },
+  {
+    path:'home',
+    component:HomeScreenComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule,
